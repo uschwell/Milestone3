0fc41a27cd0c48f7d45a5e1ace603813afc99133
Uriel Schwell <45977705+uschwell@users.noreply.github.com>
2021-06-27 22:09:19 +0200

    Delete app directory

--- app/src/main/java/MyNotifier.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class MyNotifier extends Observable {
-    public synchronized void setChanged() {
-        super.setChanged();
-    }
-    public synchronized void notifyObservers(Object args){
-        System.out.println(args);
-    }
-}
